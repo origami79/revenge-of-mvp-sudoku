1b03a5a4e1d7a5771c420cfd1cfc5038fd6382ea
origami79 <cerezo.sarah@gmail.com>
2021-12-11 14:10:06 -0500

    More CSS styling

--- client/src/components/Settings.jsx
+++ client/src/components/Settings.jsx
@@ -6,9 +6,12 @@ const Settings = (props) => {
   return (
     <div id="Settings">
       <h2>Settings</h2>
+      <form id="other-settings" onSubmit={e => e.preventDefault()}>
       <button onClick={props.solved}>Check Solution</button>
       <button>Hint</button>
+      </form>
       <form id="new-game" onSubmit={e => e.preventDefault()}>
+        <h3>New Puzzle</h3>
         <input type="radio" id="easy" name="difficulty" onClick={props.difficulty} value="easy"></input>
         <label htmlFor="easy">Easy</label><br></br>
         <input type="radio" id="medium" name="difficulty" onClick={props.difficulty} value="medium"></input>
@@ -18,6 +21,7 @@ const Settings = (props) => {
         <button onClick={props.newPuzzle}>Get New Puzzle</button>
       </form>
       <form id="pieces" onSubmit={e => e.preventDefault()}>
+        <h3>Change Style</h3>
         <input type="radio" id="number" name="piece-selection" onClick={props.pieces} value="number"></input>
         <label htmlFor="number">Numbers</label><br></br>
         <input type="radio" id="color" name="piece-selection" onClick={props.pieces} value="color"></input>
